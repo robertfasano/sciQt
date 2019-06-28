22a1612c480d7f13e8aab2300b586a630dc5467c
Robert Fasano <robert.j.fasano@colorado.edu>
2019-06-28 15:48:40 -0600

    Add pytest-qt plugin

--- sciQt/tests/test_parameter_table.py
+++ sciQt/tests/test_parameter_table.py
@@ -1,8 +1,7 @@
 from sciQt import ParameterTable, Application
 import numpy as np
 
-def test_parametertable_set_parameters():
-    app = Application()
+def test_parametertable_set_parameters(qtbot):
     set_params = {'a': np.random.uniform(), 'b': np.random.uniform()}
     table = ParameterTable(set_params)
     params = table.get_parameters()
