1f6623b0ad5c08be0cebdfa4a400ea791b361f06
Robert Fasano <robert.j.fasano@colorado.edu>
2019-06-28 13:14:27 -0600

    Add testing and CI/CD

---
+++ sciQt/tests/test_parameter_table.py
@@ -0,0 +1,10 @@
+from sciQt import ParameterTable, Application
+import numpy as np
+
+def test_parametertable_set_parameters():
+    app = Application()
+    set_params = {'a': np.random.uniform(), 'b': np.random.uniform()}
+    table = ParameterTable(set_params)
+    params = table.get_parameters()
+    for key in params:
+        assert params[key] == set_params[key]
