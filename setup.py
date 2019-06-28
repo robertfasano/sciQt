98379f9b514c4e965d8a87ed34da8cfac5b9eaf6
Robert Fasano <robert.j.fasano@colorado.edu>
2019-06-28 17:09:52 -0600

    Recursively include all /resources content in package data

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+import os
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+extra_files = package_files('sciQt/resources')
+
 setup(
     name='sciQt',
     version='0.1',
@@ -10,6 +20,7 @@ setup(
     packages=find_packages(),
     license='MIT',
     long_description=open('README.md').read(),
+    package_data={'': extra_files},
     include_package_data = True,
     install_requires=['pyqt5', 'requests', 'pint', 'numpy']
 )
