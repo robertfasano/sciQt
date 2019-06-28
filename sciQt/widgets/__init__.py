34f79c1fe8dc7f1c1d4b66499e57463108b3fe73
Robert Fasano <robert.j.fasano@colorado.edu>
2019-06-28 13:27:31 -0600

    Add __init__.py files in /tools and /widgets

---
+++ sciQt/widgets/__init__.py
@@ -0,0 +1,5 @@
+from .dashboard import Dashboard, Frame
+from .dict_menu import DictMenu
+from .icon_button import IconButton
+from .parameter_table import ParameterTable
+from .unit_edit import UnitEdit
