8c9d251b7c1ef24df732b7984f6fb5f74aa6c8fc
Robert Fasano <34036221+robertfasano@users.noreply.github.com>
2019-08-14 15:44:56 -0600

    Factor widget coloring into __init__ for simple use in other widgets

--- sciQt/widgets/color/__init__.py
+++ sciQt/widgets/color/__init__.py
@@ -1,1 +1,21 @@
+import yaml
+import os
+from sciQt import path as sciQt_path
+
+def loadPalette(self):
+    with open(os.path.join(sciQt_path, 'palette.yml')) as file:
+        palette = yaml.load(file, Loader=yaml.SafeLoader)
+    style = ''
+    for category in palette:
+        for color, hex in palette[category].items():
+            name = f'{category}: {color}'
+            style += f'QLabel[color="{name}"] {{ background-color : {hex}; }}\n'
+    self.setStyleSheet(style)
+
+def setColor(self, color):
+    self.setProperty('color', color)
+    self.style().unpolish(self)
+    self.style().polish(self)
+    self.update()
+
 from .color_label import ColorLabel
--- sciQt/widgets/color/color_label.py
+++ sciQt/widgets/color/color_label.py
@@ -1,25 +1,10 @@
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtCore import Qt
-from sciQt import path as sciQt_path
-
-import yaml
-import os
-with open(os.path.join(sciQt_path, 'palette.yml')) as file:
-    palette = yaml.load(file, Loader=yaml.SafeLoader)
+from sciQt.widgets.color import loadPalette, setColor
 
 class ColorLabel(QLabel):
     def __init__(self, text):
         super().__init__(text)
-        self.setAlignment(Qt.AlignCenter)
-        style = ''
-        for category in palette:
-            for color, hex in palette[category].items():
-                name = f'{category}: {color}'
-                style += f'QLabel[color="{name}"] {{ background-color : {hex}; }}\n'
-        self.setStyleSheet(style)
-
-    def setColor(self, color):
-        self.setProperty('color', color)
-        self.style().unpolish(self)
-        self.style().polish(self)
-        self.update()
+        loadPalette(self)
+        self.setColor = lambda color: setColor(self, color)
+        
