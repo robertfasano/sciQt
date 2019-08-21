57ee22fab7403fafcf1959ffdf064470c24ed8ca
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-08-21 15:26:58 -0600

    Fix vertical scale bug

--- sciQt/widgets/viz/sparkline.py
+++ sciQt/widgets/viz/sparkline.py
@@ -8,19 +8,19 @@ class Sparkline(QWidget):
         self.series = np.array(series)
 
     def paintEvent(self, event):
-        painter = QPainter(self)
-        xstep = self.width()/len(self.series)
-        ystep = self.height() / (np.max(self.series)-np.min(self.series))
+        if len(self.series) < 2:
+            return
 
-        x = xstep * np.linspace(0, len(self.series), len(self.series))
-        y = self.height()/2 - ystep*self.series
+        painter = QPainter(self)
+        x = self.width()/len(self.series) * np.linspace(0, len(self.series), len(self.series))
+        y = self.height()/2* (1 - (self.series-self.series.min())/(self.series.max()-self.series.min()) )
 
         painter.drawPoint(x[0], y[0])   # draw first point
         for i in range(1, len(x)):
             painter.drawLine(x[i-1], y[i-1], x[i], y[i])
         painter.end()
 
-    def update(self, data):
+    def append(self, data):
         ''' Add new data to plot '''
         self.series = np.append(self.series, data)
         self.repaint()
